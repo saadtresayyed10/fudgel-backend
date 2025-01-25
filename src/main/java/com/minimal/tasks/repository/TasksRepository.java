1df63290a3b893c652902d4f7c6b5bd6e0d5fbba
unknown <saadsyed950@gmail.com>
2025-01-25 18:00:39 +0530

    Restarting...

--- src/main/java/com/minimal/tasks/repository/TasksRepository.java
+++
@@ -1,9 +0,0 @@
-package com.minimal.tasks.repository;
-
-import com.minimal.tasks.model.Tasks;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface TasksRepository extends JpaRepository<Tasks, UUID> {
-}
--- src/main/java/com/minimal/tasks/repository/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.minimal.tasks.repository;
-
-import com.minimal.tasks.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, String> {
-}
