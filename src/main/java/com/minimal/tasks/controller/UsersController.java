0858836e61cff62d2565761890ca7f8cb5d10b95
unknown <saadsyed950@gmail.com>
2025-01-25 16:47:23 +0530

    Method to save clerk users into db defined

--- src/main/java/com/minimal/tasks/controller/UsersController.java
+++ src/main/java/com/minimal/tasks/controller/UsersController.java
@@ -6,6 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UsersController {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @PostMapping("/users")
+    public ResponseEntity<Users> createUser(@RequestBody Users users) {
+        return ResponseEntity.ok(usersRepository.save(users));
+    }
 
 }
