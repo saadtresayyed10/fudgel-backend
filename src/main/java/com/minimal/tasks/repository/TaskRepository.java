2b81224734a84df1c0c21676e1c3e3b4565f958e
unknown <saadsyed950@gmail.com>
2025-02-01 14:25:42 +0530

    Changing language

--- src/main/java/com/minimal/tasks/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.minimal.tasks.repository;
-
-import com.minimal.tasks.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
